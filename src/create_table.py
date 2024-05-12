4ddd5b5c2b503c3fe2cc6f1ddf717449de7c7bdb
Fritz Henrique <fritzhenrique.dev@gmail.com>
2024-05-12 11:42:04 -0400

    fix: improvement to use mysql

--- src/create_table.py
+++ src/create_table.py
@@ -1,3 +1,4 @@
+import asyncio
 from src.models import *
 from src.core.deps import Base
 from src.core.database import engine
@@ -5,12 +6,13 @@ from src.core.database import engine
 
 async def create_tables() -> None:
     print("Creating tables...")
-
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
 
 
 if __name__ == "__main__":
-    import asyncio
+    async def main():
+        await create_tables()
+        await engine.dispose()
 
-    asyncio.run(create_tables())
+    asyncio.run(main())
