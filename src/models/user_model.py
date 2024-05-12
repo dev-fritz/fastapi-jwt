3e3284c11072e3c788899e973508b1f9cdd88db0
Fritz Henrique <fritzhenrique.dev@gmail.com>
2024-05-12 11:40:53 -0400

    change: update model to use in mysql database

--- src/models/user_model.py
+++ src/models/user_model.py
@@ -6,6 +6,6 @@ class User(Base):
     __tablename__ = 'user'
 
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, nullable=False)
-    email = Column(String, nullable=False, unique=True)
-    password = Column(String, nullable=False)
+    name = Column(String(100), nullable=False)
+    email = Column(String(100), nullable=False, unique=True)
+    password = Column(String(100), nullable=False)
